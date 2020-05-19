6b62aabde5a0b890944d9aaccb1c16fe0ffa4770
Piotr Libera <liberapiotr1@gmail.com>
2020-05-19 18:53:23 +0200

    Move config to file

--- core/src/com/main/Config.java
+++ core/src/com/main/Config.java
@@ -1,8 +1,12 @@
 package com.main;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.Vector;
 
 public class Config {
     public static final Map<String, Integer> objectCost;
@@ -21,8 +25,22 @@ public class Config {
     public static final int startingCoins = 5000;
     public static final float refreshRate = 2f;
 
-    static boolean isUnit(String type) {
-        return type == "firstUnit";
+    static Boolean[][] createGrid(Vector<String> gridRows) {
+        if(gridRows.size() > 0) {
+            Boolean[][] grid = new Boolean[gridRows.size()][gridRows.elementAt(0).length()];
+            for(int x = 0; x < gridRows.size(); ++x) {
+                for(int y = 0; y < gridRows.elementAt(x).length(); ++y) {
+                    if(gridRows.elementAt(x).charAt(y) == '0') {
+                        grid[x][y] = false;
+                    }
+                    else {
+                        grid[x][y] = true;
+                    }
+                }
+            }
+            return grid;
+        }
+        return new Boolean[0][0];
     }
 
     static {
@@ -39,52 +57,89 @@ public class Config {
         Map<String, String> tempFullTexture = new HashMap<>();
         Map<String, Boolean[][]> tempMapGrid = new HashMap<>();
 
-        String type = "firstUnit";
-        tempObjectCost.put(type, 300);
-        tempObjectReward.put(type, 100);
-        tempWidth.put(type, 64f);
-        tempHeight.put(type, 64f);
-        tempDamage.put(type, 5);
-        tempRange.put(type, 300.0f);
-        tempReloadTime.put(type, 1.0f);
-        tempHp.put(type, 100);
-        tempSpeed.put(type, 100.0f);
-        tempRepresentativeTexture.put(type, "units/firstUnit/firstUnit00.png");
-        tempFullTexture.put(type, "units/firstUnit/firstUnit");
-
-        type = "firstTower";
-        tempObjectCost.put(type, 400);
-        tempObjectReward.put(type, 200);
-        tempWidth.put(type, 64f);
-        tempHeight.put(type, 64f);
-        tempDamage.put(type, 10);
-        tempRange.put(type, 300.0f);
-        tempReloadTime.put(type, 2.0f);
-        tempHp.put(type, 200);
-        tempRepresentativeTexture.put(type, "towers/firstTower/firstTower0.png");
-        tempFullTexture.put(type, "towers/firstTower/firstTower");
-
-        type = "missile";
-        tempSpeed.put(type, 300f);
-        tempWidth.put(type, 8f);
-        tempHeight.put(type, 8f);
-        tempRepresentativeTexture.put(type, "missile.png");
-
-        type = "map0";
-        tempRepresentativeTexture.put(type, "maps/map0.png");
-        Boolean[][] grid = {
-                {false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false},
-                {false, false, false, false, false, false, false, false, false, true, true, false, false, false, false, false, false, false, false, false}
-        };
-        tempMapGrid.put(type, grid);
+        try {
+            File configFile = new File("core/assets/config/config.txt");
+            Scanner reader = new Scanner(configFile);
+            String type = "";
+            String line;
+            String attribute;
+            String value;
+            String[] data;
+            int counter = 0;
+            boolean readingGrid = false;
+            Vector<String> gridRows = new Vector<>();
+            while (reader.hasNextLine()) {
+                counter++;
+                line = reader.nextLine().trim();
+                if(line.length() == 0) {
+                    if(readingGrid) {
+                        readingGrid = false;
+                        tempMapGrid.put(type, createGrid(gridRows));
+                    }
+                    continue;
+                }
+                if(line.charAt(0) == '#')
+                    continue;
+                data = line.split(":");
+                if(data.length == 1) {
+                    data[0] = data[0].trim();
+                    if(readingGrid) {
+                        if(data[0].charAt(0) == '0' || data[0].charAt(0) == '1') {
+                            gridRows.add(data[0]);
+                        }
+                        else {
+                            readingGrid = false;
+                            tempMapGrid.put(type, createGrid(gridRows));
+                            type = data[0];
+                        }
+                    }
+                    else if(data[0].equals("grid")) {
+                        readingGrid = true;
+                        gridRows = new Vector<String>();
+                    }
+                    else
+                        type = data[0];
+                }
+                else if(data.length == 2) {
+                    readingGrid = false;
+                    attribute = data[0].trim();
+                    value = data[1].trim();
+                    if(attribute.equals("cost"))
+                        tempObjectCost.put(type, Integer.parseInt(value));
+                    else if(attribute.equals("reward"))
+                        tempObjectReward.put(type, Integer.parseInt(value));
+                    else if(attribute.equals("width"))
+                        tempWidth.put(type, Float.parseFloat(value));
+                    else if(attribute.equals("height"))
+                        tempHeight.put(type, Float.parseFloat(value));
+                    else if(attribute.equals("damage"))
+                        tempDamage.put(type, Integer.parseInt(value));
+                    else if(attribute.equals("range"))
+                        tempRange.put(type, Float.parseFloat(value));
+                    else if(attribute.equals("reload"))
+                        tempReloadTime.put(type, Float.parseFloat(value));
+                    else if(attribute.equals("hp"))
+                        tempHp.put(type, Integer.parseInt(value));
+                    else if(attribute.equals("speed"))
+                        tempSpeed.put(type, Float.parseFloat(value));
+                    else if(attribute.equals("representativeTexture"))
+                        tempRepresentativeTexture.put(type, value);
+                    else if(attribute.equals("fullTexture"))
+                        tempFullTexture.put(type, value);
+                }
+                else {
+                    readingGrid = false;
+                    System.out.println("Syntax error found at line "+counter+" of the config file");
+                }
+            }
+            if(readingGrid) {
+                tempMapGrid.put(type, createGrid(gridRows));
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
 
         objectCost = Collections.unmodifiableMap(tempObjectCost);
         objectReward = Collections.unmodifiableMap(tempObjectReward);
