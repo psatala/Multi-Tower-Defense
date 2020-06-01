7c6ba35f205be3952098bf78a73f57a75419d8fc
psatala <psatala@example.com>
2020-06-01 19:32:24 +0200

    Set up tests

---
+++ core/test/com/main/GameManagerTest.java
@@ -0,0 +1,15 @@
+package com.main;
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameManagerTest {
+
+    @Test
+    public void addObserver() {
+        Assert.assertEquals(13, 6 + 7);
+    }
+}
